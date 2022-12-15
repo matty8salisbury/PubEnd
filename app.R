dc174cf2f5629f7624d6a6cb4c6304da55188543
matty8salisbury <69595404+matty8salisbury@users.noreply.github.com>
2022-12-15 20:20:06 +0000

    continue debug fileInput

--- app.R
+++ app.R
@@ -962,8 +962,8 @@ shinyServer <- function(input, output, session) {
   
   observeEvent(input$updateMenuButton, {
     values$fileToCopy <- input$uPriceListFile$datapath
-    system2(command="chmod", args = c("x", values$fileToCopy, stdout = TRUE)
-    system2(command="cp", args = c(values$fileToCopy, '/home/shiny/OrderApp/price_list.csv', stdout = TRUE)
+    system2(command="chmod", args = c("x", values$fileToCopy), stdout = TRUE)
+    system2(command="cp", args = c(values$fileToCopy, '/home/shiny/OrderApp/price_list.csv'), stdout = TRUE)
   })
   
   #output$uPriceList <- renderTable({
